ced42e3a3899535c08650c9e60353bebc3474658
Matthew Fidler <matthew.fidler@gmail.com>
2013-11-12 08:21:42 -0600

    Added ido-toggle-prefix back to ido-vertical-mode

--- ido-vertical-mode.el
+++ ido-vertical-mode.el
@@ -204,7 +204,8 @@ The values can be:
 (defun sd/ido-define-keys () ;; C-n/p is more intuitive in vertical layout
   (when ido-vertical-define-keys
     (define-key ido-completion-map (kbd "C-n") 'ido-next-match)
-    (define-key ido-completion-map (kbd "C-p") 'ido-prev-match))
+    (define-key ido-completion-map (kbd "C-p") 'ido-prev-match)
+    (define-key ido-completion-map (kbd "M-p") 'ido-toggle-prefix))
   (when (eq ido-vertical-define-keys 'C-n-C-p-up-and-down)
     (define-key ido-completion-map (kbd "<up>") 'ido-prev-match)
     (define-key ido-completion-map (kbd "<down>") 'ido-next-match))
